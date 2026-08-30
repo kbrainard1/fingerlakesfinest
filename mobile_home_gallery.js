59ee5549fff945fd895c410140461bdf344c0cbe
kbrainard1 <47262866+kbrainard1@users.noreply.github.com>
2026-08-29 21:35:35 -0400

    Add progress dots to mobile gallery

--- mobile_home_gallery.js
+++ mobile_home_gallery.js
@@ -1,5 +1,21 @@
 var carouselIndex = 0;
 
+function drawGallery() {
+    var dotDiv = document.getElementById("gallery_dot_progress");
+    var limit = document.getElementById("full_available_list").childElementCount
+    for (let i = 0; i < limit; i++) {
+        var newDot = document.createElement("div");
+        newDot.className = "gallery_dot";
+        dotDiv.appendChild(newDot);
+    }
+
+
+    var width = document.querySelector(".nav_box_mobile").width;
+    var remainingWidth = width - (limit * 20);
+    dotDiv.style = "margin-left: " + (remainingWidth / 2) + "px; grid-column: 1 / 3;";
+    displayImage();
+}
+
 function displayFullSize(index) {
     carouselIndex = index;
     displayImage();
@@ -27,6 +43,12 @@ function displayImage() {
     document.getElementById("mobile_home_gallery_text").textContent = text;
     document.getElementById("mobile_home_gallery_display").src = nextImage;
     document.getElementById("avail_gallery_outer").href = link;
+
+    var dotProgress = document.getElementById("gallery_dot_progress");
+    for (let i = 0; i < dotProgress.childElementCount; i++) {
+        dotProgress.children[i].style = "background-color: lightgrey;";
+    }
+    dotProgress.children[carouselIndex].style = "background-color: darkgrey;";
 }
 
 function makeSafe(bound) {
